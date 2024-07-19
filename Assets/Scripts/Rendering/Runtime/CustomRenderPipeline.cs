27ba95c07560ce87fb664472e16519a8548c4d62
apayyai <apayyai@hotmail.com>
2024-07-19 09:39:26 +0200

    Init

---
+++ Assets/Scripts/Rendering/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+using UnityEngine.Rendering;
+
+namespace Rendering.Runtime
+{
+  public partial class CustomRenderPipeline : RenderPipeline
+  {
+    private RenderGraph _renderGraph = new("Custom SRP Render Graph");
+    
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+      var renderGraphParameters = new RenderGraphParameters
+      {
+          commandBuffer = CommandBufferPool.Get(),
+          currentFrameIndex = Time.renderedFrameCount,
+          scriptableRenderContext =  context,
+          rendererListCulling = true,
+      };
+      
+      using (_renderGraph.RecordAndExecute(renderGraphParameters))
+      {
+        using RenderGraphBuilder builder = _renderGraph.AddRenderPass("Opaque", out OpaqueRenderPass pass);
+        pass.Setup(cameras[0], _renderGraph);
+        builder.SetRenderFunc<OpaqueRenderPass>(OpaqueRenderPass.RenderCallback);
+        
+      }
+      
+      _renderGraph.EndFrame();
+    }
+    
+    protected override void Dispose(bool disposing)
+    {
+      _renderGraph.Cleanup();
+      _renderGraph = null;
+    }
+  }
+}
---
+++ Assets/Scripts/Rendering/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Rendering.Runtime
+{
+  [CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+  public partial class CustomRenderPipelineAsset : RenderPipelineAsset
+  {
+
+    protected override RenderPipeline CreatePipeline()
+    {
+      return new CustomRenderPipeline();
+    }
+  }
+}
